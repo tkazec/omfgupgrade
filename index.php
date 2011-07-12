4675c1ee636d4bd0d4fbb29e0bbccd69cd66f2b8
Teddy Cross <tkazec@gmail.com>
2011-07-11 20:12:33 -0400

    First commit
    Basic style is complete.

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>OMFG, upgrade!</title>
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans">
+		<style>
+		html,body,div,span,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,abbr,address,cite,code,del,dfn,em,img,ins,kbd,q,samp,small,strong,sub,sup,var,b,i,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,figcaption,figure,footer,header,hgroup,menu,nav,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;outline:0;font-size:100%;vertical-align:baseline;background:transparent}body{line-height:1}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}nav ul{list-style:none}blockquote,q{quotes:none}blockquote:before,blockquote:after,q:before,q:after{content:'';content:none}a{margin:0;padding:0;font-size:100%;vertical-align:baseline;background:transparent}ins{background-color:#ff9;color:#000;text-decoration:none}mark{background-color:#ff9;color:#000;font-style:italic;font-weight:bold}del{text-decoration:line-through}abbr[title],dfn[title]{border-bottom:1px dotted;cursor:help}table{border-collapse:collapse;border-spacing:0}hr{display:block;height:1px;border:0;border-top:1px solid #ccc;margin:1em 0;padding:0}input,select{vertical-align:middle}
+		
+		a {
+			color: #FFF;
+			filter: alpha(opacity = 30);
+			float: left;
+			opacity: 0.3;
+			text-decoration: none;
+		}
+		a:hover {
+			filter: alpha(opacity = 100);
+			opacity: 1;
+		}
+		
+		body {
+			background: #000;
+			color: #FFF;
+			font: 16px "Open Sans", sans-serif;
+			margin: 0 auto;
+			text-align: center;
+			width: 1000px;
+		}
+		
+		h1 {
+			font-weight: 400;
+			font-size: 48px;
+			margin: 80px;
+		}
+		</style>
+	</head>
+	<body>
+		<h1>OMFG, upgrade your f-ing browser!</h1>
+		
+		<a href="http://www.opera.com"><img src="Opera.png" width="200" height="186"><br>Opera 11.5</a>
+		<a href="http://www.getfirefox.com"><img src="Firefox.png" width="200" height="186"><br>Firefox 5</a>
+		<a href="http://www.google.com/chrome" style="opacity:1"><img src="Chrome.png" width="200" height="186"><br>Chrome 12</a>
+		<a href="http://www.apple.com/safari"><img src="Safari.png" width="200" height="186"><br>Safari 5</a>
+		<a href="http://www.ie9.com"><img src="Internet Explorer.png" width="200" height="186"><br>Internet Explorer 9</a>
+	</body>
+</html>
